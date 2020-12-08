d5533c859630041d8b70af86d3dd5725fe75e869
matsuhisa <matsuhisa+github@gmail.com>
2020-12-09 00:32:03 +0900

    Use .css/.scss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import 'highlight.js/styles/tomorrow-night.css'
+import '../stylesheets/normal.css'
+import '../stylesheets/scss.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/foo.tsx
+++ pages/foo.tsx
@@ -2,15 +2,14 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const FooPage = () => (
-  <Layout title="About | Next.js + TypeScript Example">
-    <h1>Foo</h1>
-    <p>This is the foo page</p>
-    <p>
-      <Link href="/">
-        <a>Go home</a>
-      </Link>
-    </p>
-  </Layout>
+  <>
+    <div className="block">
+      block
+      <div className="block__element">
+        element
+      </div>
+    </div>
+  </>
 )
 
 export default FooPage

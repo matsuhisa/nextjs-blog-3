90727db6665d44fe8ba99c1e505a5f76fcee9745
matsuhisa <matsuhisa+github@gmail.com>
2020-12-15 23:56:50 +0900

    WIP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import 'highlight.js/styles/tomorrow-night.css'
+import { AppProps } from 'next/dist/next-server/lib/router/router'
 import '../stylesheets/normal.css'
 import '../stylesheets/scss.scss'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 

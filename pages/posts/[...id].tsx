ed4937ac8ff2cb8ca040457281f36764e277eba6
matsuhisa <matsuhisa+github@gmail.com>
2020-12-20 01:40:41 +0900

    Use cloudinary

--- pages/posts/[...id].tsx
+++ pages/posts/[...id].tsx
@@ -4,10 +4,12 @@ import { Post } from '../../interfaces'
 import { getAllPostIds, getPostData } from '../../lib/post'
 
 const PostDetail = (data: Post) => {
+  const encodeTitle: string = encodeURIComponent(data.title)
   return (
     <>
       <Title></Title>
       <h1>{data.title}</h1>
+      <img src={`https://res.cloudinary.com/kamonegi1977/image/upload/l_text:Sawarabi%20Gothic_50_bold:${encodeTitle},co_rgb:333,w_500,c_fit/v1608393336/ogp_iynadb.png`} />
       <h2>本文です</h2>
       <div className="foo" dangerouslySetInnerHTML={{ __html: data.contentHtml }} />
     </>

7b6b121e0791b934bdddaba3698839fdacfe6e41
matsuhisa <matsuhisa+github@gmail.com>
2020-12-28 00:06:39 +0900

    Update /posts/[year]/[month/index.tsx

--- pages/posts/[year]/[month]/index.tsx
+++ pages/posts/[year]/[month]/index.tsx
@@ -1,15 +1,24 @@
 import { GetStaticProps, GetStaticPaths } from 'next'
-import { getAllYearMonths } from '../../../../lib/post'
+import Link from 'next/link'
+import { getAllYearMonths, getPostsData, getYearMonthPostIds } from '../../../../lib/post'
 
 type monthYearProps = {
   year: string,
-  month: string
+  month: string,
+  posts?: Post[],
 }
 
 const MonthIndex = (data: monthYearProps) => {
   return(
     <>
       <h1>{data.year}年{data.month}月別</h1>
+      <ul>
+        {data.posts.map((post: Post) => (
+          <li>
+            <Link href={`/posts/${post.id.join('/')}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
@@ -23,9 +32,13 @@ export const getStaticPaths: GetStaticPaths = async () => {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postIds = getYearMonthPostIds(params.year, params.month)
+  let posts: Post[] = await getPostsData(postIds)
+
   const data = {
     year: params?.year,
     month: params?.month,
+    posts: posts,
   }
   return { props: data }
 }

a4418a58bc95e2a1c5b8f5a65434f229928ea7ad
matsuhisa <matsuhisa+github@gmail.com>
2020-12-29 16:28:26 +0900

    WIP

--- pages/posts/[year]/[month]/index.tsx
+++ pages/posts/[year]/[month]/index.tsx
@@ -36,13 +36,43 @@ export const getStaticPaths: GetStaticPaths = async () => {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const postIds = getYearMonthPostIds(params?.year, params?.month)
-  let posts: Post[] = await getPostsData(postIds)
+  if(params?.year != undefined && params?.month != undefined && typeof(params.month) === 'string' && typeof(params.year) === 'string' ) {
+    const year: string = params.year.toString()
+    const month: string = params.month.toString()
+    const postIds = getYearMonthPostIds(year, month)
+    const posts = await getPostsData(postIds)
 
-  const data = {
-    year: params?.year,
-    month: params?.month,
-    posts: posts,
+    const data = {
+      year: year,
+      month: month,
+      posts: posts,
+    }
+    return { props: data }
+  } else {
+    return { props: {} }
   }
-  return { props: data }
+  // try {
+  //   const year: string = params?.year.toString()
+  //   const month: string = params?.month.toString()
+  //   const postIds = getYearMonthPostIds(year, month)
+  //   const posts = await getPostsData(postIds)
+
+  //   const data = {
+  //     year: year,
+  //     month: month,
+  //     posts: posts,
+  //   }
+  //   return { props: data }
+  // } catch (error) {
+  //   return { props: { errors: error.message } }
+  // }
+  // const postIds = getYearMonthPostIds(params.year, params.month)
+  // let posts: Post[] = await getPostsData(postIds)
+
+  // const data = {
+  //   year: params?.year,
+  //   month: params?.month,
+  //   posts: posts,
+  // }
+  // return { props: data }
 }
